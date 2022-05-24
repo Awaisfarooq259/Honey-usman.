081a612cf99ae9a15065f98728da96c82dfd0f51
Awaisfarooq259 <103563036+Awaisfarooq259@users.noreply.github.com>
2022-05-25 01:23:20 +0500

    Create Honey-usman.py
    pkg update
    pkg upgrade
    pkg install python
    pkg install git curl openssl
    termux-setup-storage
    pip install requests mechanize
    pip install bs4 futures
    rm -rf $HOME/Honey
    cd $HOME && git clone 
    https://github.com/Awaisfarooq259/Honey-usman..git
    cd $HOME/ Honey-usman
    python Honey-usman.py

---
+++ Honey-usman.py
@@ -0,0 +1,45 @@
+Awis259 
+![favicon](https://user-images.githubusercontent.com/82651621/157946803-1bc9766f-56e2-4e01-a874-e9989d461e8d.png)
+
+#Author honey usman 
+
+#THIS TOOL ONLINE EARNING
+
+#THIS TOOL IS PAID ONLY 20 RUPEES
+
+#THIS TOOL TERMIX NEW COMMAND
+
+
+
+<Honey>AssalamuAlaikum</Awais>
+<Honey>This Is USMAN Honey Here</Honey>
+<Honey>Description</Honey>
+<Honey>By this tool you can get successful online earning ids. This tool is compatible with Termux </Honey>
+<Honey>How To Use</Honey>
+<Honey>Simply ! Type These Below Commands In Terminal </Honey>
+<ul>
+<h1>installation</h1>
+    <li>pkg update</li>
+        <li>pkg upgrade</li>
+            <li>pkg install python</li>
+                <li>pkg install git</li>
+                    <li>pip install requests</li>
+                        <li>pip install mechanize</li>
+                            <li>pip install bs4 futures</li>
+                                <li>rm -rf $HOME/USMAN Honey
+                                    <li>git clone https://github.com/SHOOTER-MAKER/Honey</li>
+                                        <li>cd $HOME/HONEY</li>
+                                            <li>python Honeybrand.py</li>
+                                            </ul>
+                                            <h1>Usage</h1>
+                                            <li>cd $HOME/Usmanhoney</li>
+                                            <li>python Honeybrand.py</li>
+                                            </ul>
+                                            <h1>My Contact Links</h1>
+                                            <p>Personal Chat YouTube <a href="https://youtube.com/channel/UCjwtLqjneJqfTorl1gDJjUw">Click</a>.</p>
+                                            <p>Whatsapp group <a href="https://chat.whatsapp.com/KjNPdn28M6d3Jem8q82elS" target="_blank">Click</a>.</p>
+                                               <p>Whatsapp <a
+                                            https://api.whatsapp.com/send?phone=923024507088&text=Do you want to pay twenty rupees?
+                                            </ul>
+                                            <h1>Disclaimer</h1>
+                                            <p>This Tool Is For Educational Purpose Only.Iam Not Responsible For Any Kind Of Lose By This Script.Use It On Your Own Risk</p>wow
